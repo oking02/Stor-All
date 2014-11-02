ec48acabf6877b71fdf2772ebbd80230033e1b03
oking <ollyking123@gmail.com>
2014-11-02 17:35:34 +0000

    Added get using json for single projects and reads.

--- src/main/java/server/resources/project/SingleProjectResource.java
+++ src/main/java/server/resources/project/SingleProjectResource.java
@@ -1,19 +1,30 @@
 package main.java.server.resources.project;
 
+import main.java.dto.Analysis;
 import main.java.dto.Project;
 import main.java.dto.TransferObject;
+import main.java.mysql.builder.AnalysisBuilder;
+import main.java.mysql.presenter.ExperimentPresenter;
 import main.java.mysql.presenter.ProjectPresenter;
 import main.java.mysql.remover.ProjectRemover;
 import main.java.mysql.utils.DtoToXml;
+import main.java.mysql.utils.IDGenerator;
 import main.java.mysql.utils.XMLToDto;
+import main.java.server.util.AddResponceHeaders;
 import main.java.server.util.GenericExporter;
 import main.java.server.util.ResourceExceptionHandling;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.restlet.engine.header.Header;
+import org.restlet.ext.json.JsonRepresentation;
 import org.restlet.ext.xml.DomRepresentation;
 import org.restlet.representation.Representation;
 import org.restlet.resource.Delete;
 import org.restlet.resource.Get;
 import org.restlet.resource.Put;
 import org.restlet.resource.ServerResource;
+import org.restlet.util.Series;
 import org.w3c.dom.Document;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -28,7 +39,7 @@ import static main.java.server.util.ResourceExceptionHandling.*;
  */
 public class SingleProjectResource extends ServerResource {
 
-    @Get
+    @Get("?xml")
     public Representation getProject() throws SQLException, ParserConfigurationException, IOException {
 
         int queryID = Integer.parseInt(this.getAttribute("id"));
@@ -49,6 +60,25 @@ public class SingleProjectResource extends ServerResource {
         DomRepresentation domRepresentation = new DomRepresentation();
         domRepresentation.setDocument(document);
         return domRepresentation;
+    }
+
+    @Get("?json")
+    public Representation getProjectJson() throws Exception {
+        Series<Header> responseHeaders = (Series<Header>) getResponse().getAttributes().get("org.restlet.http.headers");
+        AddResponceHeaders.addHeaders(responseHeaders, getResponse());
+
+        int queryExpID = Integer.parseInt(this.getAttribute("id"));
+        ProjectPresenter projectPresenter = new ProjectPresenter();
+        List<TransferObject> listOfProject = projectPresenter.getProject(queryExpID);
+
+        JSONArray jsonArray = new JSONArray();
+        String[] names = new String[]{"id", "owner"};
+
+        for (TransferObject transferObject : listOfProject){
+            JSONObject jsonObject1 = new JSONObject(transferObject, names);
+            jsonArray.put(jsonObject1);
+        }
+        return new JsonRepresentation(jsonArray);
     }
 
     @Put
--- src/main/java/server/resources/read/SingleReadResource.java
+++ src/main/java/server/resources/read/SingleReadResource.java
@@ -1,17 +1,24 @@
 package main.java.server.resources.read;
 
 import main.java.dto.TransferObject;
+import main.java.mysql.presenter.ProjectPresenter;
 import main.java.mysql.presenter.ReadPresenter;
 import main.java.mysql.remover.ReadRemover;
 import main.java.mysql.utils.DtoToXml;
+import main.java.server.util.AddResponceHeaders;
 import main.java.server.util.GenericExporter;
 import main.java.server.util.ResourceExceptionHandling;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.restlet.engine.header.Header;
+import org.restlet.ext.json.JsonRepresentation;
 import org.restlet.ext.xml.DomRepresentation;
 import org.restlet.representation.Representation;
 import org.restlet.resource.Delete;
 import org.restlet.resource.Get;
 import org.restlet.resource.Put;
 import org.restlet.resource.ServerResource;
+import org.restlet.util.Series;
 import org.w3c.dom.Document;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -25,7 +32,7 @@ import static main.java.server.util.ResourceExceptionHandling.*;
  */
 public class SingleReadResource extends ServerResource {
 
-    @Get
+    @Get("?xml")
     public Representation getRead() throws ParserConfigurationException, IOException {
 
         int queryID = Integer.parseInt(this.getAttribute("id"));
@@ -46,6 +53,25 @@ public class SingleReadResource extends ServerResource {
         domRepresentation.setDocument(document);
         return domRepresentation;
 
+    }
+
+    @Get("?json")
+    public Representation getReadJson() throws Exception {
+        Series<Header> responseHeaders = (Series<Header>) getResponse().getAttributes().get("org.restlet.http.headers");
+        AddResponceHeaders.addHeaders(responseHeaders, getResponse());
+
+        int queryExpID = Integer.parseInt(this.getAttribute("id"));
+        ReadPresenter readPresenter = new ReadPresenter();
+        List<TransferObject> listOfRead = readPresenter.getRead(queryExpID);
+
+        JSONArray jsonArray = new JSONArray();
+        String[] names = new String[]{"id"};
+
+        for (TransferObject transferObject : listOfRead){
+            JSONObject jsonObject1 = new JSONObject(transferObject, names);
+            jsonArray.put(jsonObject1);
+        }
+        return new JsonRepresentation(jsonArray);
     }
 
     @Put
