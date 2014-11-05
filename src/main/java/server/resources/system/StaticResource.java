ff1e50c14c327a2e3f98e0343776ec788b5e8d34
oking <ollyking123@gmail.com>
2014-11-05 23:01:49 +0000

    Successfully got Stor-All to server the static html and js pages and display correctly.

---
+++ src/main/java/server/resources/system/StaticResource.java
@@ -0,0 +1,38 @@
+package main.java.server.resources.system;
+
+import main.java.server.util.AddResponceHeaders;
+import org.restlet.data.MediaType;
+import org.restlet.engine.header.Header;
+import org.restlet.representation.FileRepresentation;
+import org.restlet.representation.Representation;
+import org.restlet.resource.Get;
+import org.restlet.resource.ServerResource;
+import org.restlet.util.Series;
+
+import java.io.File;
+
+/**
+ * Created by oking on 05/11/14.
+ */
+public class StaticResource extends ServerResource {
+
+    @Get
+    public Representation getIndexPage(){
+
+        Series<Header> responseHeaders = (Series<Header>) getResponse().getAttributes().get("org.restlet.http.headers");
+        AddResponceHeaders.addHeaders(responseHeaders, getResponse());
+
+        FileRepresentation fileRepresentation = new FileRepresentation(new File("/home/oking/Documents/PersonalSite/BootStrapVersion/index.html"), MediaType.TEXT_HTML);
+        return fileRepresentation;
+    }
+
+    @Get("?js")
+    public Representation getJSPage(){
+
+        Series<Header> responseHeaders = (Series<Header>) getResponse().getAttributes().get("org.restlet.http.headers");
+        AddResponceHeaders.addHeaders(responseHeaders, getResponse());
+
+        FileRepresentation fileRepresentation = new FileRepresentation(new File("/home/oking/Documents/PersonalSite/BootStrapVersion/scripts.js"), MediaType.APPLICATION_JAVASCRIPT);
+        return fileRepresentation;
+    }
+}
