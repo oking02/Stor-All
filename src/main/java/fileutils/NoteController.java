4d7faf688c094ee77d75b1f0d2928065bfb66469
oking <ollyking123@gmail.com>
2014-11-05 14:34:49 +0000

    Added a note taking class. Will eventually allow clients to add notes to Experiments, Reads and Projects. Stores the notes in a simple .txt file.

---
+++ src/main/java/fileutils/NoteController.java
@@ -0,0 +1,49 @@
+package main.java.fileutils;
+
+import main.java.dto.TransferObject;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by oking on 05/11/14.
+ */
+public class NoteController {
+    private int id;
+    private String type;
+
+    public NoteController(int id, String type) throws NoSuchFieldException {
+        this.id = id;
+        this.type = type;
+    }
+
+    public void addNotes(String message) throws IOException {
+
+        File notesFile = new File(new File("").getAbsolutePath() +"/" + type + "s/" + type + "-" + id +"/notes.txt");
+        System.out.println(notesFile.getAbsolutePath());
+        if (!doesNotFileExist(notesFile)){
+            notesFile.createNewFile();
+        }
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+
+        FileWriter fw = new FileWriter(notesFile, true);
+        fw.write(System.getProperty("line.separator") + "Note Entry -- " + dateFormat.format(date));
+        fw.write(System.getProperty("line.separator"));
+        fw.write(System.getProperty("line.separator") + message);
+        fw.write(System.getProperty("line.separator") + "------------------------------");
+        fw.write(System.getProperty("line.separator"));
+        fw.close();
+    }
+
+    private boolean doesNotFileExist(File file){
+        return file.exists();
+    }
+
+
+}
